62a18a7e51e15d2a4aefb4c3fc442343d1f0df2a
David Amador <54488581+dsamador@users.noreply.github.com>
2022-01-05 14:37:52 -0500

    Cadena de conexion

---
+++ LeerData/AppVentaCursosContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace LeerData
+{
+    /* Una instancia de DbContext representa una sesion con la base de datos a la que nos queremos conectar
+    en pocas palabras es la base de datos */
+    /* Un DdSet representa una vista o una tabla de la base de datos */
+    public class AppVentaCursosContext : DbContext
+    {
+        
+    }
+}
